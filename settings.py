c5ba0cfc7fbddc9d003fc3189135415e52358e5c
Rainny <118473233+wimol123@users.noreply.github.com>
2024-06-18 02:04:02 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -34,13 +34,13 @@ VIDEOS_DICT = {
 }
 
 # ML Model config
-MODEL_DIR = ROOT / 'weights'
-DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+MODEL_DIR = ROOT / 'model'
+DETECTION_MODEL = MODEL_DIR / 'mymodelyolo.pt'
 # In case of your custome model comment out the line above and
 # Place your custom model pt file name at the line below 
 # DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
 
-SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+#SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
 # Webcam
 WEBCAM_PATH = 0
