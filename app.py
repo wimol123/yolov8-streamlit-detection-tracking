f42dee1655beb5e756d1a7a3296ddcb432fa2599
Rainny <118473233+wimol123@users.noreply.github.com>
2024-06-18 01:52:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,19 @@ st.title("Object Detection And Tracking using YOLOv8")
 model_path = Path(settings.DETECTION_MODEL)
 model = helper.load_model(model_path)
 
+st.title("HAPPY RAINNY KANOMTHAI..")
+image = st.file_uploader("Choose .jpg pic ...", type=["png", "jpg", "jpeg"])
+if image:
+    image = Image.open(image)
+    st.image(image=image)
+
+    st.write("")
+    st.write("Detecting...")
+
+    result = model(image)
+    names = result[0].names
+    probability = result[0].probs.data.numpy()
+    prediction = np.argmax(probability)
+    className = int(names[prediction])
+    className = class_labels_names[className]
+    st.write(className)
